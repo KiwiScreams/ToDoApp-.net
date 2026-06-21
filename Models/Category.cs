70407962e76a92c188e07feb592abf3abf993312
KiwiScreams <screamskiwi3@gmail.com>
2026-06-21 09:05:26 +0400

    Models/TaskItem Model

--- Models/Category.cs
+++ Models/Category.cs
@@ -5,7 +5,7 @@ namespace ToDoApp_final.Models
     public class Category
     {
         public int Id { get; set; }
-        public string Name { get; set; } = string.Empty;
+        public string Name { get; set; }
         public List<TaskItem> Tasks { get; set; } = new();
     }
 }
