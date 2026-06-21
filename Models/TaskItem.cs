c8809c810ecda33064964c4798f1743a97a5cb42
KiwiScreams <screamskiwi3@gmail.com>
2026-06-21 08:55:44 +0400

    Models/User Model

---
+++ Models/TaskItem.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ToDoApp_final.Models
+{
+    public class TaskItem
+    {
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ToDoApp_final.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public List<TaskItem> Tasks { get; set; } = new();
+    }
+}
